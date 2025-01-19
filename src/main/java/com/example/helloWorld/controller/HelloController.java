b40f7c3c3e66e823be0300745c7d884eacd4b6a9
Sagar Melmatti <sagaramelmatti@gmail.com>
2025-01-19 12:55:16 +0530

    Update HelloController.java

--- src/main/java/com/example/helloWorld/controller/HelloController.java
+++ src/main/java/com/example/helloWorld/controller/HelloController.java
@@ -5,16 +5,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 
+ * This controller handles greetings.
  */
 @RestController
 @RequestMapping("/api/hello")
 public class HelloController {
-	
-	/**
-	 * @return
-	 */
-	@GetMapping("/")
+
+    /**
+     * Returns a greeting message.
+     *
+     * @return A "Hello World!" message.
+     */
+    @GetMapping("/")
     public String sayHello() {
         return "Hello World!";
     }
