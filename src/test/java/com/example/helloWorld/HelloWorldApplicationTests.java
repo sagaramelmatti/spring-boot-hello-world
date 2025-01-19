028846308538e5afba12890d2a9d8837398a4e83
Sagar Melmatti <sagaramelmatti@gmail.com>
2025-01-19 13:05:12 +0530

    Update HelloWorldApplicationTests.java

--- src/test/java/com/example/helloWorld/HelloWorldApplicationTests.java
+++ src/test/java/com/example/helloWorld/HelloWorldApplicationTests.java
@@ -1,14 +1,18 @@
 package com.example.helloWorld;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.helloWorld.controller.HelloController;
+
 @SpringBootTest
 class HelloWorldApplicationTests {
-	/*
 	@Test
-	void contextLoads() {
-	}
-	*/
-
+    public void testSayHello() {
+        HelloController controller = new HelloController();
+        String response = controller.sayHello();
+        assertThat(response).isEqualTo("Hello World!");
+    }
 }
